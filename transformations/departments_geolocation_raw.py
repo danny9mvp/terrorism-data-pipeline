97f8b33aebaa4e7b8e975ce20a83bee6339555d6
danny9mvp <danny9mvp@gmail.com>
2026-06-09 12:41:49 -0500

    Revert "feature: Removed geo department data preparation"
    This reverts commit d4281fa664f484147b3093547d32e038f1d0faa9.

---
+++ transformations/departments_geolocation_raw.py
@@ -0,0 +1,30 @@
+from pyspark import pipelines as dp
+from pyspark.sql.types import FloatType, IntegerType, StringType, StructField, StructType
+
+from utils.datos_abiertos_client import RestClient
+from utils.file_utils import FileUtils
+from utils.file_service import FileService
+
+file_path = f"{spark.conf.get("divipola.file.path")}"
+divipola_url = f"{spark.conf.get("resources.url")}{spark.conf.get("divipola.url.file.name")}"
+
+client = RestClient()
+file_utils = FileUtils()
+file_service = FileService(client, file_utils)
+
+schema = StructType([
+    StructField("COD_DPTO", IntegerType(), True),
+    StructField("NOM_DPTO", StringType(), True),
+    StructField("COD_MPIO", IntegerType(), True),
+    StructField("NOM_MPIO", StringType(), True),
+    StructField("TIPO", StringType(), True),
+    StructField("LATITUD", FloatType(), True),
+    StructField("LONGITUD", FloatType(), True),
+    StructField("GEO_MUNICIPIO", StringType(), True)
+])
+
+
+@dp.table(comment="Raw data of the colombian department coordinates")
+def departments_geo_raw():
+    file_service.write_csv_file(divipola_url, file_path)
+    return spark.read.csv(file_path, header=True, schema=schema)    
